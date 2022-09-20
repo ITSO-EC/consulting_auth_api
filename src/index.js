e776aa03f898684bbf1a83ca4a6f2e1b2dbebb03
JoelVeloz <joevpazm@espol.edu.ec>
2022-09-19 23:39:47 -0500

    updt

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ const logger = require('./config/logger');
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
+  // server = app.listen(config.port, () => {
+  //   logger.info(`Listening to port ${config.port}`);
+  // });
+  server = app.listen('/home/joel/web/auth.itso.ga/nodeapp/app.sock', () => {
     logger.info(`Listening to port ${config.port}`);
   });
 });
